41008816825446bc9acc4a797ac6b9844cdb44da
Mohammed Alhaddar <moalhaddar@gmail.com>
2024-10-19 11:05:20 +0300

    Added wrapping

--- emoji.sh
+++ emoji.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(dirname "$(readlink -f $0)")
-OUTPUT=$(cat "$BASEDIR/emojies.txt" | fzf --tiebreak=chunk,begin --print-query)
+OUTPUT=$(cat "$BASEDIR/emojies.txt" | fzf --tiebreak=chunk,begin --print-query --wrap)
 QUERY=$(echo "$OUTPUT" | sed -n '1p')
 EMOJI=$(echo "$OUTPUT" | sed -n '2p' | cut -d " " -f 1)
 echo "$EMOJI" | tr -d "\n" | xclip -selection clipboard && sleep 0.01
